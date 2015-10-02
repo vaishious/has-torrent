df665414d08d79a41b588c7ba41365641dbbdd3a
Pranav Vaish <pranav.vaish@gmail.com>
2015-10-02 06:04:27 +0530

    Connect Request working

---
+++ src/UDPTracker.hs
@@ -0,0 +1,53 @@
+import Network.Socket hiding (send, sendTo, recv, recvFrom)
+import Network.Socket.ByteString
+import Network.BSD
+import qualified Data.ByteString.Lazy.Char8 as BL
+import Data.ByteString.Builder
+import Data.Binary
+import Data.Int
+import Data.Word
+import Data.Monoid
+import Data.Maybe
+import System.Timeout
+import System.Random
+import Control.Monad
+
+makeSockAddr :: String -> Int -> IO SockAddr
+makeSockAddr trackerName trackerPort = do tracker <- getHostByName trackerName
+                                          return $ SockAddrInet (fromIntegral trackerPort) (hostAddress tracker)
+
+makeSocket :: IO Socket
+makeSocket = socket AF_INET Datagram 17
+
+sockConnTracker :: String -> Int -> IO Socket
+sockConnTracker trackerName trackerPort = do trackerSock <- makeSockAddr trackerName trackerPort
+                                             sockUDP <- makeSocket
+                                             connect sockUDP trackerSock
+                                             return sockUDP
+
+connectConnId :: Int64
+connectConnId = 4497486125440
+
+connectAction :: Int32
+connectAction = 0
+
+genTransactionId :: IO Int32
+genTransactionId = randomIO
+
+sendConnectRequest :: Int -> Socket -> IO (Maybe Int64)
+sendConnectRequest timeWait sockUDP = do transactionId <- genTransactionId
+                                         send sockUDP $ BL.toStrict $ toLazyByteString $ (int64BE connectConnId) `mappend` (int32BE connectAction) `mappend` (int32BE transactionId)
+                                         maybeResponse <- timeout (timeWait*1000000) (recv sockUDP 16)
+                                         print maybeResponse
+                                         return $ liftM BL.fromStrict maybeResponse >>= (parseConnectResponse transactionId)
+
+parseConnectResponse :: Int32 -> BL.ByteString -> (Maybe Int64)
+parseConnectResponse transactionId response = if BL.length response < 16
+                                                 then Nothing
+                                                 else let (responseAction,responseTransConn) = BL.splitAt 4 response
+                                                      in if (decode responseAction :: Int32) /= connectAction
+                                                            then Nothing
+                                                            else let (responseTrans,responseConn) = BL.splitAt 4 responseTransConn
+                                                                 in if (decode responseTrans :: Int32) /= transactionId
+                                                                       then Nothing
+                                                                       else Just (decode responseConn :: Int64)
