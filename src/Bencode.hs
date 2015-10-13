95b0d08da954e4e6d456b901e0daff24c9e75f05
Sundararajan R <dyoucme@gmail.com>
2015-10-13 12:38:04 +0530

    Rectified CoveredFileList computation and modified some function names to reflect their semantics

--- src/Bencode.hs
+++ src/Bencode.hs
@@ -19,17 +19,20 @@ import Control.Monad (replicateM)
 import System.Random
 import Data.Word
 
+lenHash :: Int
+lenHash = 20
+
 readAndDecode :: FilePath -> IO (Maybe BEncode)
 readAndDecode fp = do bs <- B.readFile fp
                       return $ bRead bs
 
-infoHash :: Maybe BEncode -> Maybe B.ByteString
-infoHash (Just (BDict be)) = Just $ B.fromStrict $ toBytes $ hashlazy $ bPack infoDict
+findInfoHash :: Maybe BEncode -> Maybe B.ByteString
+findInfoHash (Just (BDict be)) = Just $ B.fromStrict $ toBytes $ hashlazy $ bPack infoDict
                            where Just infoDict = M.lookup "info" be
-infoHash _ = Nothing
+findInfoHash _ = Nothing
 
-getFileList :: Maybe BEncode -> Maybe BEncode
-getFileList = successiveLookup ["info","files"]
+readFileDict :: Maybe BEncode -> Maybe BEncode
+readFileDict = successiveLookup ["info","files"]
 
 getFiles :: BEncode -> [([FilePath],Integer)]
 getFiles (BList (BDict map:xs)) = (pathlist,len) : getFiles (BList xs)
@@ -50,7 +53,7 @@ successiveLookup _ _ = Nothing
 splitPieceHash :: B.ByteString -> [B.ByteString]
 splitPieceHash piecehash = case B.length piecehash > 0 of
                                True -> head : splitPieceHash piecehash'
-                                    where (head,piecehash') = B.splitAt 20 piecehash
+                                    where (head,piecehash') = B.splitAt (fromIntegral lenHash) piecehash
                                _    -> []
 
 pieceHashList :: Maybe BEncode -> Maybe [B.ByteString]
@@ -61,8 +64,8 @@ pieceHashList be = case successiveLookup ["info","pieces"] be of
 getOverallSize :: FileList -> Integer
 getOverallSize = V.foldr ((+) . getSize) 0
 
-getPieceLength :: Maybe BEncode -> Maybe Integer
-getPieceLength be = case successiveLookup ["info","piece length"] be of
+readPieceLength :: Maybe BEncode -> Maybe Integer
+readPieceLength be = case successiveLookup ["info","piece length"] be of
                       (Just (BInt len)) -> Just len
                       _                 -> Nothing
 
@@ -70,8 +73,8 @@ pieceLengthList :: Integer -> Integer -> [Int]
 pieceLengthList totalSize pieceLength = replicate (fromIntegral $ quot totalSize pieceLength) (fromIntegral pieceLength) ++ last
                                       where last = if mod totalSize pieceLength == 0 then [] else [fromIntegral $ mod totalSize pieceLength]
 
-pieceInfo :: [Int] -> [B.ByteString] -> FileList -> PieceInfo
-pieceInfo lenList hashList fileList = V.fromList $ zipWith3 SinglePieceInfo lenList (map Hash hashList) (coveredFileList fileList (map fromIntegral (0:lenList)))
+setPieceInfo :: [Int] -> [B.ByteString] -> FileList -> PieceInfo
+setPieceInfo lenList hashList fileList = V.fromList $ zipWith3 SinglePieceInfo lenList (map Hash hashList) (coveredFileList fileList (map fromIntegral lenList))
 
 -- What if input is not of the type (BList a)?
 announceURL :: BEncode -> String
@@ -90,8 +93,8 @@ uriToTracker uri = case filter isLetter $ uriScheme parsedURI of
                                   where auth = fromJust $ uriAuthority parsedURI
                  where parsedURI = fromJust $ parseURI uri
 
-getTrackerList :: [String] -> TrackerList
-getTrackerList = map uriToTracker
+readTrackerList :: [String] -> TrackerList
+readTrackerList = map uriToTracker
 
 runningSum :: [Integer] -> [Integer]
 runningSum = scanl1 (+)
@@ -127,19 +130,20 @@ coveredFileList fileList pieceLengthList = map g $ allSplit filePositionList $ p
 toFile :: FilePath -> ([FilePath],Integer) -> File
 toFile rootPath (listPath,size) = File (fullFoldPath rootPath listPath) size
 
-fileList :: FilePath -> [([FilePath],Integer)] -> FileList
-fileList rootPath files = V.fromList $ map (toFile rootPath) files
+readFileList :: FilePath -> [([FilePath],Integer)] -> FileList
+readFileList rootPath files = V.fromList $ map (toFile rootPath) files
 
 genPeerID :: IO B.ByteString
 genPeerID = do let randomWord8 = getStdRandom random :: IO Word8
-               word8List <- replicateM 20 randomWord8
+               word8List <- replicateM lenHash randomWord8
                return $ B.pack word8List
 
 setStateless :: FilePath -> FilePath -> IO (Maybe Stateless)
 setStateless torrentFile rootPath = do be             <- readAndDecode torrentFile
-                                       peerid         <- genPeerID
-                                       let infohash    = Hash $ fromJust $ infoHash be
-                                       let trackerList = getTrackerList $ fromJust $ announceList be
-                                       let filelist    = fileList rootPath (getFiles (fromJust (Bencode.getFileList be)))
-                                       let pieceinfo   = pieceInfo (pieceLengthList (getOverallSize filelist) (fromJust $ Bencode.getPieceLength be)) (fromJust $ pieceHashList be) filelist
-                                       return $ Just $ Stateless infohash pieceinfo (Hash peerid) trackerList filelist
+                                       peerID         <- genPeerID
+                                       let infoHash    = Hash $ fromJust $ findInfoHash be
+                                       let trackerList = readTrackerList $ fromJust $ announceList be
+                                       let fileList    = readFileList rootPath (getFiles (fromJust $ readFileDict be))
+                                       let pieceLenList = pieceLengthList (getOverallSize fileList) (fromJust $ readPieceLength be)
+                                       let pieceInfo   = setPieceInfo pieceLenList (fromJust $ pieceHashList be) fileList
+                                       return $ Just $ Stateless infoHash pieceInfo (Hash peerID) trackerList fileList
