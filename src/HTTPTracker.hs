a6fe0fdbfacca8bfaeb5c26ed221c870c4a0e05c
Aadi Yogakar <ayogakar@gmail.com>
2015-10-29 11:09:52 +0530

    changed acc to Types.hs

--- src/HTTPTracker.hs
+++ src/HTTPTracker.hs
@@ -1,9 +1,13 @@
+module HTTPTracker where
+import Types
+import TypesHelp
 import System.IO
 import Network.URL
 import Network.HTTP
 import Control.Applicative
 import Control.Monad
-import qualified Data.ByteString.UTF8 as U
+import qualified Data.ByteString.Lazy as BL
+import qualified Language.Haskell.TH.Ppr as L
 
 addParam :: Maybe URL -> (String,String) -> Maybe URL
 addParam url (key,val) = case url of
@@ -20,10 +24,10 @@ getParam port up dw left event com num
         | com == 0 = [("port",show port),("uploaded",show up),("downloaded",show dw),("left",show left),("event",event),("numwant",show num)]
         | otherwise = [("port",show port),("uploaded",show up),("downloaded",show dw),("left",show left),("event",event),("compact","1"),("numwant",show num)]
 
-getUrl :: String -> U.ByteString -> U.ByteString -> String
-getUrl tracker infoHashEsc peerIdEsc = tracker ++ "?" ++ U.toString infoHashEsc ++ "&" ++ U.toString peerIdEsc
+getUrl :: String -> BL.ByteString -> BL.ByteString -> String
+getUrl tracker infoHashEsc peerIdEsc = tracker ++ "?" ++ L.bytesToString (BL.unpack infoHashEsc) ++ "&" ++ L.bytesToString (BL.unpack peerIdEsc)
 
-requestUrl :: String -> U.ByteString -> U.ByteString -> Integer -> Integer -> Integer -> Integer -> String -> Integer -> Integer -> Maybe URL
+requestUrl :: String -> BL.ByteString -> BL.ByteString -> Integer -> Integer -> Integer -> Integer -> String -> Integer -> Integer -> Maybe URL
 requestUrl tr infoHashEsc peerIdEsc port up dw left event com num = z where
         tracker = getUrl tr infoHashEsc peerIdEsc
         param = getParam port up dw left event com num
@@ -39,9 +43,22 @@ checkResponse url = do
 first :: (Eq a, Eq b, Eq c) => (a,b,c) -> a
 first (a,b,c) = a
 
-getResponse :: Maybe URL -> IO (Maybe String)
-getResponse u = case u of
-        Just url -> checkResponse url
+eventToString :: Event -> String
+eventToString e = case decodeEvent e of 1 -> "completed"
+                                        3 -> "stopped"
+                                        _ -> "started"
+
+checkResult :: Maybe URL -> IO (Maybe String)                                        
+checkResult url = case url of
+        Just u -> checkResponse u
         Nothing -> return Nothing
 
---main will have the form of getResponse $ requestUrl input
+getResponse :: Tracker -> Stateless -> Torrent -> IO (Maybe String)
+getResponse httpTracker constants stateful = checkResult url where
+                                                        url = requestUrl tracker infoHashEsc peerIdEsc 6881 0 dw left event 1 50
+                                                        tracker = getURL httpTracker
+                                                        infoHashEsc = getHash (getInfoHash constants)
+                                                        peerIdEsc = getHash (getPeerId constants)
+                                                        dw = getDownload $ getPieces stateful
+                                                        left = getLeft $ getPieces stateful
+                                                        event = eventToString $ getEvent stateful
