cead8b333dbdbb4e2c9d493cb9a8d01b06addffa
Pranav Vaish <pranav.vaish@gmail.com>
2015-11-03 01:19:00 +0530

    Code for receiving and processing messages for active peers

--- src/PeerProtocol.hs
+++ src/PeerProtocol.hs
@@ -2,6 +2,7 @@ module PeerProtocol where
 import File
 import Types
 import TypesHelp
+import Communicate
 import Control.Concurrent
 import Control.Monad.State
 import Control.Monad.Writer
@@ -9,7 +10,10 @@ import Crypto.Hash.SHA1
 import Data.Byteable
 import Data.ByteString.Builder
 import Data.Int
+import Data.Maybe
 import qualified Data.Vector as V
+import qualified Data.List.Zipper as Z
+import qualified Data.List as L
 import Data.Word
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Set as S
@@ -18,6 +22,7 @@ import Network.Socket hiding (send, sendTo, recv, recvFrom)
 import Network.Socket.ByteString.Lazy
 import System.Timeout
 import Control.Applicative
+import Data.Time
 
 pStr :: String
 pStr = "BitTorrent protocol"
@@ -71,14 +76,14 @@ erasePieceData piece = piece{ getBlocks = M.map eraseBlockData $ getBlocks piece
 setVerifiedStatus :: Piece -> Piece
 setVerifiedStatus piece = piece{ getVerifiedStatus = True }
 
-verifyHashAndWrite :: Int -> Stateless -> StateT Torrent IO Bool
+verifyHashAndWrite :: Int -> Stateless -> StateT Torrent IO ()
 verifyHashAndWrite index constants = StateT $ \torrent -> if computedHash torrent index == expectedHash constants index
                                                           then do forkIO $ writePiece index constants torrent
                                                                   let pieces = (V.//) (getPieces torrent) [(index,setVerifiedStatus $ getPieces torrent V.! index)]
-                                                                  return (True,torrent{ getPieces = pieces })
+                                                                  return ((),torrent{ getPieces = pieces })
                                                           else do let pdo = getPieceDownOrd torrent
                                                                   let pieces = (V.//) (getPieces torrent) [(index,erasePieceData $ getPieces torrent V.! index)]
-                                                                  return (False,torrent{ getPieceDownOrd = pdo ++ [index], getPieces = pieces })
+                                                                  return ((),torrent{ getPieceDownOrd = pdo ++ [index], getPieces = pieces })
 
 checkAndAddPieces :: StateT Torrent IO ()
 checkAndAddPieces = do torrent <- get
@@ -89,3 +94,51 @@ checkAndAddPieces = do torrent <- get
                             let newBlocks = pieceToReqs x $ getPieces torrent
                             put torrent{getPieceDownOrd = xs, getActiveBlocks = S.union (getActiveBlocks torrent) newBlocks}
                             checkAndAddPieces
+
+activePeer :: Stateless -> StateT Torrent IO ()
+activePeer constants = do torrent <- get
+                          if Z.endp $ getActivePeers torrent
+                              then do put torrent{getActivePeers = Z.start $ getActivePeers torrent}
+                                      -- TODO: inactivePeer
+                              else do let peer = Z.cursor $ getActivePeers torrent
+                                      peer <- lift $ recvDataPeer peer
+                                      put torrent{getActivePeers = Z.replace (execState peerMessages peer) $ getActivePeers torrent}
+                                      processActiveMessages constants
+                                      -- TODO: Check if inactive and send the required messages
+
+recvdBlockData :: Message -> Stateless -> StateT Torrent IO ()
+recvdBlockData msg constants = do torrent <- get
+                                  let peer = Z.cursor $ getActivePeers torrent
+                                  when (getPieceIndex msg < V.length (getPieces torrent)) $ do
+                                      let piece = getPieces torrent V.! getPieceIndex msg
+                                      let maybeBlock = M.lookup (getBlockBegin msg) $ getBlocks piece
+                                      unless (isNothing maybeBlock) $ do
+                                          let block = fromJust maybeBlock
+                                          let recvId = RequestId (getPieceIndex msg,getBlockBegin msg)
+                                          when (elem recvId $ getRequestList peer) $ do
+                                              let peer = peer{getRequestList = L.delete recvId $ getRequestList peer}
+                                              when (S.member recvId $ getActiveBlocks torrent) $ do
+                                                  let block = block {getDownloadStatus = True, getData = getBlock msg}
+                                                  let piece = piece {getBlocks = M.adjust (const block) (getBlockBegin msg) $ getBlocks piece}
+                                                  put torrent{getActiveBlocks = S.delete recvId $ getActiveBlocks torrent,
+                                                              getActivePeers = Z.replace peer $ getActivePeers torrent,
+                                                              getPieces = getPieces torrent V.// [(getPieceIndex msg,piece)]}
+                                                  when (M.foldr (\a b -> getDownloadStatus a && b) True $ getBlocks piece) $ verifyHashAndWrite (getPieceIndex msg) constants
+
+processActiveMessages :: Stateless -> StateT Torrent IO ()
+processActiveMessages constants = do torrent <- get
+                                     let peer = Z.cursor $ getActivePeers torrent
+                                     let messages = getPendingMessages peer
+                                     unless (null messages) $ do
+                                         let (msg:rest) = messages
+                                         time <- lift getCurrentTime
+                                         let peer = peer {getPendingMessages = rest, getResponseTime = time}
+                                         case msg of ChokeMsg -> do let peer = peer {getEffResponseTime = time}
+                                                                    put torrent {getActivePeers = Z.delete $ getActivePeers torrent,
+                                                                                 getInactivePeers = Z.push peer $ getInactivePeers torrent}
+                                                                    return ()
+                                                     PieceMsg{} -> do let peer = peer {getEffResponseTime = time}
+                                                                      put torrent {getActivePeers = Z.replace peer $ getActivePeers torrent}
+                                                                      recvdBlockData msg constants
+                                                                      processActiveMessages constants
+                                                     _ -> processActiveMessages constants
