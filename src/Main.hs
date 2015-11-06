a6f530522ddc9e50cbdb55662138a13edca671f5
Sundararajan R <dyoucme@gmail.com>
2015-11-06 21:32:25 +0530

    Initiating the torrent client from Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,7 @@ import TypesHelp
 
 import Data.Maybe
 import Control.Monad
+import Control.Monad.State
 import System.Environment
 import System.IO
 import System.Log.Logger
@@ -21,3 +22,14 @@ main = do fh <- fileHandler "has-torrent.log" DEBUG >>= \lh -> return $ setForma
           sh <- streamHandler stderr DEBUG >>= \lh -> return $ setFormatter lh (simpleLogFormatter "[$time : $loggername : $prio] $msg")
           updateGlobalLogger "HasTorrent" (setHandlers [fh, sh])
           updateGlobalLogger "HasTorrent" (setLevel INFO)
+          argList <- getArgs
+          let torrentFile = head argList
+          maybeConstants <- setStateless torrentFile
+          let constants = fromJust maybeConstants
+          torrent <- setStateful constants
+          execStateT (startTorrent constants) torrent
+          return ()
+
+startTorrent :: Stateless -> StateT Torrent IO ()
+startTorrent constants = do findAndAddPeers constants
+                            download constants
